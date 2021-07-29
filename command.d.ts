573ad0fbce4789b88cf5409712e1d24f652b2691
queicherius <queicherius@gmail.com>
2021-07-29 03:52:56 +0000

    Remove prettierignore

--- command.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    namespace Cypress {
-        interface Chainable {
-            savePagePerformanceMetrics(label: string): void;
-        }
-    }
-}
-export declare function addSavePagePerformanceMetricsCommand(): void;
